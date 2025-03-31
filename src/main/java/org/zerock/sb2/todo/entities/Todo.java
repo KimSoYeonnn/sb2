4f387502ae9fc5bf764887d929c559033a759774
KimSoYeonnn <10sosokim10@gmail.com>
2025-03-31 11:54:22 +0900

    create table

--- src/main/java/org/zerock/sb2/todo/entities/Todo.java
+++ src/main/java/org/zerock/sb2/todo/entities/Todo.java
@@ -1,4 +1,4 @@
-package org.zerock.sb2.todo;
+package org.zerock.sb2.todo.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,21 +10,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+//테이블 용도 
+//@Table 생략시에는 클래스 이름이 테이블 이름 
 @Entity(name = "todo")
 @Table(name = "tbl_todo")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Todo {
-    
-    //모든 엔티티는 반드시 id가 존재
-    //
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long tno;
 
-    @Column(nullable = false, length = 300)
-    private String title;
+  //모든 엔티티는 반드시 Id가 존재 
+  //PK는 반드시 객체타입(기본자료형 사용불가)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long tno;
+  
+  @Column(nullable = false, length = 300)
+  private String title;
 
-    private String writer;
+  private String writer;
+  
 }
