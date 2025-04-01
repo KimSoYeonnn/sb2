27958c23cc3a3c60d5b8223d0f394e502141ea6b
KimSoYeonnn <10sosokim10@gmail.com>
2025-04-01 17:11:28 +0900

    Paging

--- src/main/java/org/zerock/sb2/board/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/sb2/board/service/BoardServiceImpl.java
@@ -1,5 +1,26 @@
 package org.zerock.sb2.board.service;
 
+import org.springframework.stereotype.Service;
+import org.zerock.sb2.board.dto.BoardListDTO;
+import org.zerock.sb2.board.dto.PageRequestDTO;
+import org.zerock.sb2.board.dto.PageResponseDTO;
+import org.zerock.sb2.board.repository.BoardRepository;
+
+import groovy.util.logging.Log4j2;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+@Log4j2
 public class BoardServiceImpl implements BoardService {
-    
+
+    private final BoardRepository repository;
+
+    @Override
+    public PageResponseDTO<BoardListDTO> list(PageRequestDTO requestDTO) {
+        
+        return repository.list(requestDTO);
+    }
 }
